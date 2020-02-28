f62db3ded8d96643116295cc43fd119407d76db1
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-02-28 10:10:52 -0500

    site complete for now

--- react_portfolio/src/MoGraphProjects.js
+++ react_portfolio/src/MoGraphProjects.js
@@ -1,13 +1,27 @@
 import React from 'react';
+let buildData = require('./data.json');
 
-
-const About = () => {
+const MoGraphProjects = () => {
 
     return(
-        <section className="container animated slideInRight">
-            <h1>MoGraph!</h1>
+        <section className="row animated slideInRight justify-content-center">
+            <div className="col-12 rounded itemCard">
+                <h2>Motion Graphics and Video Projects</h2>
+                <p>I've always found motion graphics and VFX, as well as video editing, to be interesting and inspiring. Anything you imagine is possible. MoGraph is a hobby of mine and most of the examples below were created as a way to learn different tools in After Effects. However, with these projects and my web projects I make full use of the Adobe Creative Suite, especially Photoshop and Illustrator.</p>
+                <p>Check out my tumblr repository for more than what's listed below:</p>
+                <a href="https://lightninthorman.tumblr.com/" target="_blank" className="btn btn-outline-dark">More Projects</a>
+            </div>
+            {buildData.mographProjects.map((item,index) => {
+                return(
+                    <div key={index} className="col-4 m-3 rounded itemCard">
+                        <iframe src={item.link}  frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                    </div>
+                )
+            })}
         </section>
     )
 }
 
-export default About;
+export default MoGraphProjects;
--- react_portfolio/src/Resume.js
+++ react_portfolio/src/Resume.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 
-const About = () => {
+const Resume = () => {
 
     return(
-        <section className="container animated slideInRight">
-            <h1>Resume!</h1>
+        <section className="row animated slideInRight justify-content-center mb-3">
+            <div className="col-12 rounded itemCard">
+                <h2>Resume</h2>
+                <p>While I'm newer to the web development career path I come with years of professional experience in the fast paced, ever changing pharmaceutical industry. Besides a strong work ethic I have built equally important career skills such as time management, leadership, and developing interpersonal relationships within a team and across global partnerships. If anything, having a chance to start a new career makes me even more eager to learn as much as possible and become a valuable asset quickly.</p>
+                <p>Click on the button below to download my resume or visit my LinkedIn profile:</p>
+                <div>
+                    <a href="images/ArthurKuivanenResume.pdf" className="btn btn-outline-dark mx-4" download>Resume</a>
+                    <a  href="https://www.linkedin.com/in/arthur-kuivanen/" target="_blank"><img src="https://img.icons8.com/metro/40/000000/linkedin.png" /></a>
+                </div>
+            </div>
         </section>
     )
 }
 
-export default About;
+export default Resume;
